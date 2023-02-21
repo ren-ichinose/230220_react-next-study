0a90e940c541346b388be06915a50426b65b2361
Ren Ichinose <kamatani.toru@gmail.com>
2023-02-21 19:32:23 +0900

    コンポーネントの学習開始

--- react-next-study_01/my-app/src/pages/index.tsx
+++ react-next-study_01/my-app/src/pages/index.tsx
@@ -1,7 +1,20 @@
 const TopPage = () => {
   return (
     <div>
-      Hello, world!
+      <h1>国の首都リスト</h1>
+      <div>日本 ... 東京</div>
+      <div>アメリカ ... ワシントンD.C.</div>
+      <div>中国 ... 北京</div>
+      <div>イギリス ... ロンドン</div>
+
+      <h1>都道府県庁所在地リスト</h1>
+      <div>東京都 ... 新宿区</div>
+      <div>北海道 ... 札幌市</div>
+      <div>宮城県 ... 仙台市</div>
+      <div>愛知県 ... 名古屋市</div>
+      <div>大阪府 ... 大阪市</div>
+      <div>広島県 ... 広島市</div>
+      <div>福岡県 ... 福岡市</div>
     </div>
   )
 }
