83363aaa979c99e120a944c13b01e8758eef2eea
Ren Ichinose <kamatani.toru@gmail.com>
2023-02-21 21:39:22 +0900

    props.childrenを使ってみよう

--- react-next-study_01/my-app/src/components/CountryCapitalList.tsx
+++ react-next-study_01/my-app/src/components/CountryCapitalList.tsx
@@ -5,34 +5,56 @@ const countryCapitals = [
   { country: 'イギリス', capital: 'ロンドン' },
 ];
 
+export const Card = (props) => {
+  return (
+    <div
+      style={{ 
+        borderRadius: 3,
+        boxShadow: "0 0 3px #555",
+        display: "inline-block",
+        width: '160px',
+        padding: "13px 15px",
+        margin: "3px 5px",
+      }}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+const CountryCapitalItem = (props) => {
+  return (
+    <>
+      <div style={{
+          fontSize: "0.7em",
+        }}
+      >
+        {props.country}
+      </div>
+      <div style={{
+          fontSize: "1.1em",
+          fontWeight: "bold",
+        }}
+      >              
+        {props.capital}
+      </div>
+    </>
+  );
+};
+
 const CountryCapitalList = () => {
   return (
     <div>
       <h1>国の首都リスト</h1>
-      {countryCapitals.map(countryCapitals => {
+      {countryCapitals.map(countryCapital => {
         return (
-          <div key={countryCapitals.country}
-          style={{ 
-            borderRadius: 3,
-            boxShadow: "0 0 3px #555",
-            display: "inline-block",
-            width: '160px',
-            padding: "13px 15px",
-            margin: "3px 5px",
-          }}>
-            <div style={{
-                fontSize: "0.7em",
-            }}>
-              {countryCapitals.country}
-            </div>
-            <div style={{
-                fontSize: "1.1em",
-                fontWeight: "bold",
-              }}>              
-              {countryCapitals.capital}
-            </div>
-          </div>
-        )
+          <Card key={countryCapital.country}>
+            <CountryCapitalItem
+              country={countryCapital.country}
+              capital={countryCapital.capital}
+            />
+          </Card>
+        );
       })}
     </div>
   );
